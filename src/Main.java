41e9a71058c95040cd9efdc657af6bf64f116710
Ramon van Sprundel <ramones156@users.noreply.github.com>
2020-01-14 14:53:51 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-//        String connectionUrl =
-//                "jdbc:sqlserver://localhost:1433;integratedSecurity=true";
-//        try {
-//            Connection connection = DriverManager.getConnection(connectionUrl);
-//            Statement statement = connection.createStatement();
-//
+        String connectionUrl =
+                "jdbc:sqlserver://localhost:1433;integratedSecurity=true";
+        try {
+            Connection connection = DriverManager.getConnection(connectionUrl);
+            Statement statement = connection.createStatement();
+
 //             Class die de sql query bouwt
-//            String sqlCode = "use test;" +
-//                    "select * from aa";
-//
-//            ResultSet rs = statement.executeQuery(sqlCode);
-//
-//            while (rs.next())
-//                System.out.println(rs.getString("lol"));
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+            String sqlCode = "use test;" +
+                    "select * from aa";
+
+            ResultSet rs = statement.executeQuery(sqlCode);
+
+            while (rs.next())
+                System.out.println(rs.getString("lol"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         Application.launch(LoginPanel.class, args);
     }
 }
